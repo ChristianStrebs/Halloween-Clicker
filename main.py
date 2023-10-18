66278e07de50b6b458c531dd81261b2e48512e36
ChristianStrebs <christianstreby@gmail.com>
2023-10-18 09:56:48 -0500

    Start

---
+++ main.py
@@ -0,0 +1,23 @@
+import pygame
+
+
+
+pygame.init()
+main_clock = pygame.time.Clock()
+height = 1080
+width = 1920
+screen = pygame.display.set_mode((width, height))
+background = pygame.image.load("assets\\background.jpg")
+
+def main_menu():
+    while True:
+        screen.fill(0,0,0)
+
+def play():
+    pygame.display.set_caption("Play")
+
+
+def instructions():
+    
+pygame.seispla.set_acaopipatkioion()""pl""()""Instructions
+    screen.blit()
